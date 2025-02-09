d9c247776616ffeb9c9524f23acb14615bffea80
unknown <ing.adrianedbonilla@gmail.com>
2025-02-09 11:07:50 -0500

    Inicio del proyecto

---
+++ src/main/java/org/example/Empleado.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public abstract class Empleado {
+    private String nombre =  null;
+    private  int edad = 0;
+    private double salarioBase = double;
+    private String departamento = null;
+
+    public abstract double calcularSalario();
+
+    public void mostrarDetalles (){}
+
+}
