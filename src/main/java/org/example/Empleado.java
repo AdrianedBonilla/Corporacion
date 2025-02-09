eb4b124e268d1e05f02fb020775693a416c4c3c4
unknown <ing.adrianedbonilla@gmail.com>
2025-02-09 11:11:41 -0500

    Ajuste variable salarioBase

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -3,11 +3,12 @@ package org.example;
 public abstract class Empleado {
     private String nombre =  null;
     private  int edad = 0;
-    private double salarioBase = double;
+    private double salarioBase = 0;
     private String departamento = null;
 
     public abstract double calcularSalario();
 
-    public void mostrarDetalles (){}
+    public void mostrarDetalles (){
 
+    }
 }
