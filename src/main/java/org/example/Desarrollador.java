434c2cecdbcef9398ed35d24f7748a323958d027
unknown <ing.adrianedbonilla@gmail.com>
2025-02-09 11:31:57 -0500

    Creación metodos Desarrollador

--- src/main/java/org/example/Desarrollador.java
+++ src/main/java/org/example/Desarrollador.java
@@ -3,9 +3,33 @@ package org.example;
 public class Desarrollador extends Empleado {
     private String lenguajeProgramacion = null;
 
-    public void programar(){
+    public Desarrollador(String nombre, int edad, double salarioBase, String departamento, String lenguajeProgramacion) {
+        super(nombre, edad, salarioBase, departamento);
+        this.lenguajeProgramacion = lenguajeProgramacion;
     }
 
+    public String getLenguajeProgramacion() {
+        return lenguajeProgramacion;
+    }
+
+    public void setLenguajeProgramacion(String lenguajeProgramacion) {
+        this.lenguajeProgramacion = lenguajeProgramacion;
+    }
+
+    @Override
+    public double calcularSalario(){
+        return getSalarioBase();
+    }
+
+    public void programar(){
+        System.out.println(getNombre() + "Está programado en " + getLenguajeProgramacion());
+    }
+
+    @Override
     public  void mostrarDetalles(){
+        System.out.println("Desarrollador");
+        super.mostrarDetalles();
+        System.out.println("El lengiaje de programacion; " + getLenguajeProgramacion());
+        System.out.println("\n------------------\n");
     }
 }
