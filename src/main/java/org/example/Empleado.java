61bacec8a1d92eea24cc3cc873d348bb8b53929d
unknown <ing.adrianedbonilla@gmail.com>
2025-02-09 11:26:07 -0500

    Creación metodos Empleado-Gerente

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -6,9 +6,51 @@ public abstract class Empleado {
     private double salarioBase = 0;
     private String departamento = null;
 
+    public Empleado(String nombre, int edad, double salarioBase, String departamento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salarioBase = salarioBase;
+        this.departamento = departamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
     public abstract double calcularSalario();
 
     public void mostrarDetalles (){
-
+        System.out.println("Nombre: " +  nombre + "\n" +
+                            ",Edad: " + edad + "\n" +
+                            ",Salario: " + salarioBase + "\n" +
+                            ",Departamento: " + departamento + "\n" + ".");
     }
 }
--- src/main/java/org/example/Gerente.java
+++ src/main/java/org/example/Gerente.java
@@ -3,10 +3,27 @@ package org.example;
 public class Gerente extends Empleado{
     private double bono=0;
 
+    public Gerente(String nombre, int edad, double salarioBase, String departamento, double bono) {
+        super(nombre, edad, salarioBase, departamento);
+        this.bono = bono;
+    }
+
+    public double getBono() {
+        return bono;
+    }
+
+    public void setBono(double bono) {
+        this.bono = bono;
+    }
+    @Override
     public double calcularSalario(){
-        return 1;
+        return  getSalarioBase() + getBono();
     }
 
     public void mostrarDetalles(){
+        System.out.println("Gerente");
+        super.mostrarDetalles();
+        System.out.println("Bono: " + getBono());
+        System.out.println("\n------------------------------------\n");
     }
 }
