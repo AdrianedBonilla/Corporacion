9c1d37675dbfc386ef8c3b0181064a7f886702b4
unknown <ing.adrianedbonilla@gmail.com>
2025-02-09 11:18:12 -0500

    Creación clases Desarrollador y Gerente

---
+++ src/main/java/org/example/Desarrollador.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Desarrollador extends Empleado {
+    private String lenguajeProgramacion = null;
+
+    public void programar(){
+    }
+
+    public  void mostrarDetalles(){
+    }
+}
---
+++ src/main/java/org/example/Gerente.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Gerente extends Empleado{
+    private double bono=0;
+
+    public double calcularSalario(){
+        return 1;
+    }
+
+    public void mostrarDetalles(){
+    }
+}
